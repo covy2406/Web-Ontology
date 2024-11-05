809fc3417cd75314a6e6d15b782d4c00a30e3d18
covy2406 <nguyentranthaovy2406@gmail.com>
2024-11-05 21:08:01 +0700

    change content ui

--- src/components/RelativeCard/index.jsx
+++ src/components/RelativeCard/index.jsx
@@ -17,21 +17,22 @@ const RelativeCard = () => {
             The Ontology Lookup Service (OLS) is a repository for biomedical ontologies that aims to
             provide a single point of access to the latest ontology versions. You can browse the
             ontologies through the website as well as programmatically via the OLS API. OLS is
-            developed and maintained by the <span className="text-[#106462] underline cursor-pointer">Samples, Phenotypes and Ontologies Team (SPOT)</span> 
-            at <span className="text-[#106462] underline cursor-pointer">EMBL-EBI</span>.
+            developed and maintained by the 
+            <span className="text-[#106462] underline cursor-pointer">QPVN Team</span>  at 
+            <span className="text-[#106462] underline cursor-pointer">DUT University</span>.
           </p>
         </div>
         <div className="p-4 mx-4 bg-white border-solid border-4 border-[#106462] rounded-lg ">
           <div className="flex items-center">
             <BuildIcon sx={{ color: "#cccc34" }} />
             <h2 className="text-[#106462] ml-2 text-2xl font-semibold hover:underline cursor-pointer">
-              Related Tools
+              Biomedical ontologies?
             </h2>
           </div>
           <p className="mt-2 font-light text-left">
-            In addition to OLS the SPOT team also provides the <span className="text-[#106462] underline cursor-pointer">OxO</span> and <span className="text-[#106462] underline cursor-pointer">ZOOMA</span>  services. OxO
-            provides cross-ontology mappings between terms from different ontologies. ZOOMA is a
-            service to assist in mapping data to ontologies in OLS.
+            Biomedical ontologies organize complex medical terms and relationships, enabling data
+            sharing and analysis across systems. This improves research, healthcare, and supports AI
+            in personalized medicine.
           </p>
         </div>
         <div className="p-4 mx-4 bg-white border-solid border-4 border-[#106462] rounded-lg">
@@ -43,8 +44,13 @@ const RelativeCard = () => {
           </div>
           <p className="mt-2 font-light text-left">
             For feedback, enquiries or suggestion about OLS or to request a new ontology please use
-            our <span className="text-[#106462] underline cursor-pointer">GitHub issue tracker</span>. For announcements relating to OLS, such as new releases and
-            new features sign up to the <span className="text-[#106462] underline cursor-pointer">OLS announce mailing list</span>.
+            our 
+            <span className="text-[#106462] underline cursor-pointer">GitHub issue tracker</span>.
+            For announcements relating to OLS, such as new releases and new features sign up to the 
+            <span className="text-[#106462] underline cursor-pointer">
+              OLS announce mailing list
+            </span>
+            .
           </p>
         </div>
       </div>
